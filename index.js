e7ad32060c8089dec4c5a9d8f8155dcae388f01d
Dinesh Swami <dinesh907989@gmail.com>
2026-06-08 18:45:44 +0530

    add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  return res.json({ messsage: "Hello, tisri duniya!" , status: "github actions is working fine"});
+  return res.json({ messsage: "Hello, tisri duniya!" , status: "again try to test" });
 });
 
 app.listen(PORT, () => {
